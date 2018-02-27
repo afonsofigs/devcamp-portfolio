dc7c0df413bba969117c02d07a41d73026818a40
afonsofigs <afonso.figuinha@gmail.com>
2018-02-27 03:51:53 +0000

    Implemented edir and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,6 +21,23 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    # on rails c, you can test every db search
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The Record is successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 
 #private
 #  def portfolio_params
