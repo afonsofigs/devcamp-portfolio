f6962f2bdbadca3f1cac568497c94d5dd4257a03
afonsofigs <afonso.figuinha@gmail.com>
2018-03-02 23:58:43 +0000

    Implemented content tag helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,10 @@ module ApplicationHelper
 
   end
 
+  def source_helper(layout_name)
+     if session[:source] 
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p,greeting, class: "source-greeting")
+     end 
+  end
 end
